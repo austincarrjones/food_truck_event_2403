848b329587c70b9b3f5ad7cba233326ad8e50a8f
Austin Carr-Jones <austincarr.jones@gmail.com>
2024-04-15 11:41:50 -0400

    feat: item class

--- lib/item.rb
+++ lib/item.rb
@@ -1,11 +1,13 @@
-class item
+class Item
 
-  attr_reader
+  attr_reader :name,
+              :price
 
   def initialize(info_hash)
     @info_hash = info_hash
     @name = info_hash[:name]
-    @price = info_hash[:price]
+    @price = info_hash[:price].gsub('$', '').to_f
   end
 
+  
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,7 @@
 require 'pry'
-require '.lib/event'
-require '.lib/food_truck'
-require '.lib/item'
+require './lib/event'
+require './lib/food_truck'
+require './lib/item'
 
 RSpec.configure do |config|
   config.formatter = :documentation
